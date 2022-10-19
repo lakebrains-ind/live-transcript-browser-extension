585f4103dfbc26d01e45002c0d8646081d47d630
Your Name <ayushi.lakebrains@gmail.com>
2022-10-19 10:18:46 +0530

    oauth authentication

--- background.js
+++ background.js
@@ -1,28 +1,42 @@
-// chrome.action.onClicked.addListener(function() {
-//     chrome.tabs.create({url: 'index.html'});
-//   });
-console.log("hello");
-let windowId;
-chrome.action.onClicked.addListener(() => {
-    chrome.windows.create(
-      {
+// // chrome.action.onClicked.addListener(function() {
+// //     chrome.tabs.create({url: 'index.html'});
+// //   });
+// console.log("hello");
+// let windowId;
+// chrome.action.onClicked.addListener(() => {
+//     chrome.windows.create(
+//       {
+//         focused: true,
+//         width: 600,
+//         height: 1100,
+//         type: "popup",
+//         url: "index.html",
+//         top: 0,
+//         left: 0,
+        
+//       },
+//       (window) => {
+//         //   console.log(window.tabs[0].windowId);
+//         windowId = window.tabs[0].windowId;
+//         chrome.storage.local.set({ windowId }, function (response) {
+//           if (chrome.runtime.lastError) console.log(chrome.runtime.lastError);
+//           console.log(response);
+//         });
+//         console.log("windowId", windowId);
+//       }
+//     );
+// });
+chrome.action.onClicked.addListener(function() {
+  chrome.windows.create(
+    {
         focused: true,
         width: 600,
         height: 1100,
         type: "popup",
-        url: "index1.html",
+        url: "index.html",
         top: 0,
         left: 0,
-        
-      },
-      (window) => {
-        //   console.log(window.tabs[0].windowId);
-        windowId = window.tabs[0].windowId;
-        chrome.storage.local.set({ windowId }, function (response) {
-          if (chrome.runtime.lastError) console.log(chrome.runtime.lastError);
-          console.log(response);
-        });
-        console.log("windowId", windowId);
-      }
-    );
+
+    })
+  //chrome.tabs.create({url: 'index.html'});
 });
---
+++ oauth.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+        fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`)
+        .then((response)=> response.json())
+        .then((response) => console.log(response))      
+        //chrome.tabs.create({url: 'index1.html'});
+        chrome.windows.create(
+            {
+                focused: true,
+                width: 600,
+                height: 1100,
+                type: "popup",
+                url: "index1.html",
+                top: 0,
+                left: 0,
+
+            })
+
+      });
+    });
+  };
